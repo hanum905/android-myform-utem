d06a7f6e34855822c67cbd0edb70f597d7c3f296
Hanum <hanum905@gmail.com>
2018-08-14 16:45:25 +0800

    Add Menu

--- app/src/main/java/my/edu/utem/myform/MainActivity.java
+++ app/src/main/java/my/edu/utem/myform/MainActivity.java
@@ -1,13 +1,89 @@
 package my.edu.utem.myform;
 
+import android.app.ActionBar;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText nameEditText, emailEditText, phoneEditText, cellEditText, messageEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nameEditText = findViewById(R.id.main_nameEditText);
+        emailEditText = findViewById(R.id.main_emailEditText);
+        phoneEditText = findViewById(R.id.main_phoneEditText);
+        cellEditText = findViewById(R.id.main_cellEditText);
+        messageEditText = findViewById(R.id.main_messageEditText);
+    }
+
+
+    public void buttonPresed(View view) {
+        //link from source page to destination page
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        intent.putExtra("name", nameEditText.getText().toString());
+        intent.putExtra("email", emailEditText.getText().toString());
+        intent.putExtra("phone", phoneEditText.getText().toString());
+        intent.putExtra("cell", cellEditText.getText().toString());
+        intent.putExtra("message", messageEditText.getText().toString());
+
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_about_us:
+                Intent intent = new Intent(MainActivity.this, WebActivity.class);
+                break;
+            case R.id.menu_contact_us:
+                Toast.makeText(MainActivity.this, "Can contact us anytime", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.menu_share:
+                AlertDialog.Builder alertDialogueBuilder = new AlertDialog.Builder(MainActivity.this);
+                alertDialogueBuilder.setTitle("Are you sure?");
+                alertDialogueBuilder.setMessage("Are you sure you want to share this app?");
+                alertDialogueBuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+                alertDialogueBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                }
+            });
+
+
+        alertDialogueBuilder.show();
+        break;
+        case R.id.menu_exit:
+        finish();
+        break;
+
+    }
+        return super.
+
+    onOptionsItemSelected(item);
+
+}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
